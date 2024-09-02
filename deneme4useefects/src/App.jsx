f42dcf144bc58694f2ea6efa6dfa19233bace31a
Renestimu <byumitsiz@gmail.com>
2024-09-02 20:14:49 +0300

    düzeltme

--- deneme4useefects/src/App.jsx
+++ deneme4useefects/src/App.jsx
@@ -1,18 +1,34 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
-  useEffect(()=>{
-    console.log('useEffect')
-  })
+  useEffect(() => {
+    console.log("her zaman");
+  });
+  useEffect(() => {
+    console.log("ilk render edildiğinde");
+  }, []);
+
+  useEffect(() => {
+    // console.log("ilk render edildiğinde  ve firstname state değiştiğinde");
+    console.log("ilk render edildiğinde  ve  state değiştiğinde");
+  }, [firstName,lastName]);
+
+  // useEffect(() => {
+  //   console.log("ilk render edildiğinde  ve lastname state değiştiğinde");
+  // }, [lastName]);
   return (
     <>
-     <div>use effect</div>
+      <div>
+        <div> <button onClick={()=>setFirstName("Merhaba")}> Adı değiştir</button></div>
+        <div> <button onClick={()=>setLastName("Dünya")}> Soyadı değiştir</button></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
